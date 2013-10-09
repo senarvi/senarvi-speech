3b0f06c2efa6c52bc1787b985ac69c8da2dcd13e
Seppo Enarvi <seppo.git@marjaniemi.com>
2013-10-10 00:54:02 +0300

    Tell the most frequent unseen words in the end.

--- oov-stats/oov-stats.py
+++ oov-stats/oov-stats.py
@@ -7,6 +7,28 @@ from optparse import OptionParser
 import sys
 import io
 
+def print_stats(counts, word_limit):
+	num_test_words = sum(counts.values())
+	num_oov_words = num_test_words
+
+	vocabulary = set()
+	word_count = 0
+	sys.stdout.write("corpus size, unique words, unseen words, OOV rate\n")
+	for line in io.TextIOWrapper(sys.stdin.buffer, 'utf-8'):
+		for word in line.split():
+			if not word in vocabulary:
+				vocabulary.add(word)
+				if word in counts:
+					num_oov_words -= counts[word]
+					del counts[word]
+			word_count += 1
+			if word_count % 100000 == 0:
+				oov_rate = float(num_oov_words) / num_test_words
+				sys.stdout.write("%i, %i, %i, %f\n" % (word_count, len(vocabulary), num_oov_words, oov_rate))
+				sys.stderr.write("word_count=%i, vocabulary=%i, num_oov_words=%i, oov_rate=%f\n" % (word_count, len(vocabulary), num_oov_words, oov_rate))
+			if word_count > word_limit:
+				return
+
 parser = OptionParser()
 parser.add_option('--limit',
                   action='store', type='int', dest='limit', default=None)
@@ -25,22 +47,11 @@ with open(args[0], 'r', encoding='utf-8') as f:
 			else:
 				counts[word] += 1
 
-num_test_words = sum(counts.values())
-num_oov_words = num_test_words
+print_stats(counts, options.limit)
 
-vocabulary = set()
-word_count = 0
-sys.stdout.write("corpus size, unique words, unseen words, OOV rate\n")
-for line in io.TextIOWrapper(sys.stdin.buffer, 'utf-8'):
-	for word in line.split():
-		if not word in vocabulary:
-			vocabulary.add(word)
-			if word in counts:
-				num_oov_words -= counts[word]
-		word_count += 1
-		if word_count % 100000 == 0:
-			oov_rate = float(num_oov_words) / num_test_words
-			sys.stdout.write("%i, %i, %i, %f\n" % (word_count, len(vocabulary), num_oov_words, oov_rate))
-			sys.stderr.write("word_count=%i, vocabulary=%i, num_oov_words=%i, oov_rate=%f\n" % (word_count, len(vocabulary), num_oov_words, oov_rate))
-		if word_count > options.limit:
-			sys.exit(0)
+sorted_counts = sorted(counts, key=counts.get)
+sys.stderr.write("Most frequent unseen words:\n")
+for i in range(1, min(11, len(counts))):
+	word = sorted_counts[-i]
+	count = counts[word]
+	sys.stderr.write(word + ", " + str(count) + " occurrences\n")
