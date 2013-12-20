13107282a0b5ffa0abfb2bd391bcfc3cf244855c
Seppo Enarvi <seppo.git@marjaniemi.com>
2013-12-20 19:18:38 +0200

    Works with files that are too big to fit into memory.

--- filter-text/shuffle-pages.py
+++ filter-text/shuffle-pages.py
@@ -15,6 +15,7 @@ from filetypes import TextFileType
 parser = argparse.ArgumentParser()
 parser.add_argument('input', type=TextFileType('r'), nargs='+', help='input text page files')
 parser.add_argument('--output', type=TextFileType('w'), default='-', help='output file for reordered text pages')
+parser.add_argument('--in-memory', action='store_true', default=False, help='load the entire data set into memory')
 args = parser.parse_args()
 
 uris = set()
@@ -23,15 +24,30 @@ for input_file in args.input:
 	for page in read_pages(input_file):
 		uri = page.uri()
 		uris.add(uri)
-		if uri in pages:
-			pages[uri].add_content(page.content())
-		else:
-			pages[uri] = page
-sys.stderr.write("%i pages in the input files.\n" % len(uris))
+		if args.in_memory:
+			if uri in pages:
+				pages[uri].add_content(page.content())
+			else:
+				pages[uri] = page
+
+num_pages = len(uris)
+sys.stderr.write("%i pages in the input files.\n" % num_pages)
 
 ordered_uris = list(uris)
-random.shuffle(ordered_uris)
 
-for uri in ordered_uris:
-	args.output.write(pages[uri].header())
-	args.output.write(pages[uri].content())
+previous_progress = -1
+while len(ordered_uris) > 0:
+	index = random.randint(0, len(ordered_uris) - 1)
+	uri = ordered_uris[index]
+	del ordered_uris[index]
+	progress = int((num_pages - len(ordered_uris)) * 100 / num_pages)
+	if progress > previous_progress:
+		sys.stderr.write("%i %% done.\n" % progress)
+		previous_progress = progress
+	if args.in_memory:
+		args.output.write(pages[uri].header())
+		args.output.write(pages[uri].content())
+		args.output.flush()
+	else:
+		for input_file in args.input:
+			write_matching_content(input_file, args.output, uri)
