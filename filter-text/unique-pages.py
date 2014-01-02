f530025523bdfabf1c5a677db65de8ad7d3380b4
Seppo Enarvi <seppo.git@marjaniemi.com>
2014-01-02 23:00:25 +0200

    By default don't write headers.

--- filter-text/unique-pages.py
+++ filter-text/unique-pages.py
@@ -14,6 +14,7 @@ from filetypes import TextFileType
 parser = argparse.ArgumentParser()
 parser.add_argument('input', type=TextFileType('r'), nargs='+', help='input text page files')
 parser.add_argument('--output', type=TextFileType('w'), default='-', help='output file for reordered text pages')
+parser.add_argument('--write-headers', action='store_true', default=False, help='write also page headers so that the pages can be further filtered')
 args = parser.parse_args()
 
 seen_pages = set()
@@ -21,6 +22,7 @@ for input_file in args.input:
 	for page in read_pages(input_file):
 		if not page.content() in seen_pages:
 			seen_pages.add(page.content())
-			args.output.write(page.header())
+			if args.write_headers:
+				args.output.write(page.header())
 			args.output.write(page.content())
 			args.output.flush()
