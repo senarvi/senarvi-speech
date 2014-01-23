e717e6f9433f33c4f96029571f405e025679c51f
Seppo Enarvi <seppo.git@marjaniemi.com>
2014-01-23 19:17:40 +0200

    --exclude option.

--- filter-text/select-pages.py
+++ filter-text/select-pages.py
@@ -19,6 +19,7 @@ from filetypes import TextFileType
 
 parser = argparse.ArgumentParser()
 parser.add_argument('list', type=TextFileType('r'), help='a file containing a page identifier on each line')
+parser.add_argument('--exclude', action='store_true', default=False, help='exclude the identified pages (the default is to include only those pages)')
 parser.add_argument('--output', type=TextFileType('w'), default='-', help='output filtered text pages')
 parser.add_argument('--pages', type=TextFileType('r'), default='-', help='input text pages')
 parser.add_argument('--write-headers', action='store_true', default=False, help='write also page headers so that the pages can be further filtered')
@@ -32,6 +33,8 @@ for line in args.pages:
 	if line.startswith("###### "):
 		uri = line[7:].rstrip()
 		include = uri in select_uris
+		if args.exclude:
+			include = not include
 		if args.write_headers and include:
 			args.output.write(line)
 	elif include:
